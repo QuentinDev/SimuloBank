5cd35e1e9f2e0231230d0857ec0833de61389eac
Quentin BETTOUM <quentin.bettoum@epitech.eu>
2017-03-21 14:03:38 +0100

    Fix

---
+++ src/main/java/fr/instic/cadeaux/dao/AccountDao.java
@@ -0,0 +1,7 @@
+package fr.instic.cadeaux.dao;
+
+/**
+ * Created by Quentin on 21/03/2017.
+ */
+public interface AccountDao {
+}
---
+++ src/main/java/fr/instic/cadeaux/dao/impl/AccountDaoImpl.java
@@ -0,0 +1,11 @@
+package fr.instic.cadeaux.dao.impl;
+
+import fr.instic.cadeaux.dao.AccountDao;
+import org.springframework.stereotype.Repository;
+
+/**
+ * Created by Quentin on 21/03/2017.
+ */
+@Repository
+public class AccountDaoImpl implements AccountDao {
+}
